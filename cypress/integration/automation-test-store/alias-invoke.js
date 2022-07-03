f4de1866fb01819924746b47f48aef44ac55b3e7
jgrabanera <jgrabanera@gmail.com>
2022-07-04 05:43:22 +0800

    update alias and invoke with iteration and variables

--- cypress/integration/automation-test-store/alias-invoke.js
+++ cypress/integration/automation-test-store/alias-invoke.js
@@ -17,7 +17,7 @@ describe("Alias and invoke", () => {
   })
 
   //validate if the number of .thumbnail class is exact 16
-  it.only("Validate product thumbnail", () => {
+  it("Validate product thumbnail", () => {
     cy.visit("https://automationteststore.com/")
 
     //Invoke = extract
@@ -30,5 +30,62 @@ describe("Alias and invoke", () => {
       .should("include", "Add to Cart") //include or contain may work
 
     cy.log("Alias and invoke challenge is completed")
+  })
+
+  it.only("Calculate total of normal and sale products", () => {
+    cy.visit("https://automationteststore.com/")
+
+    //.as() is use a variable
+    cy.get(".thumbnail").as("productThumbnail")
+    // cy.get("@productThumbnail").find(".oneprice").each(($el, index, $list) => {
+    //     cy.log($el.text())
+    //   })
+
+    // Get total number of non-sale price
+    cy.get(".thumbnail") // get * element with class .thumbnail
+      .find(".oneprice") // find .oneprice class within .thumbnail
+      .invoke("text") //extract all the value produce
+      .as("itemPrice") // save in alias(variable) @itemPrice
+
+    let itemsTotal = 0
+    cy.get("@itemPrice").then(($linkText) => {
+      //callback fn
+      let itemPrice = $linkText.split("$") // split the extracted value and return as an array of itemPrice
+      let itemsTotalPrice = 0
+      for (let i = 0; i < itemPrice.length; i++) {
+        // cy.log(itemPrice[i])
+        itemsTotalPrice += Number(itemPrice[i])
+      }
+
+      itemsTotal += itemsTotalPrice
+      cy.log(
+        "Total non-sale price of " +
+          itemPrice.length +
+          " items is: $" +
+          itemsTotal
+      )
+    })
+
+    // Get total number of Sale price
+    cy.get(".thumbnail").find(".pricenew").invoke("text").as("saleItemPrice")
+
+    let saleItemsTotal = 0
+    cy.get("@saleItemPrice").then(($linkText) => {
+      //callback fn
+      let saleItemPrice = $linkText.split("$") // split the extracted value and return as an array of itemPrice
+      let salesItemsTotalPrice = 0
+      for (let i = 0; i < saleItemPrice.length; i++) {
+        // cy.log(saleItemPrice[i])
+        salesItemsTotalPrice += Number(saleItemPrice[i])
+      }
+
+      saleItemsTotal += salesItemsTotalPrice
+      cy.log(
+        "Total sale price of " +
+          saleItemPrice.length +
+          " items is: $" +
+          saleItemsTotal
+      )
+    })
   })
 })
