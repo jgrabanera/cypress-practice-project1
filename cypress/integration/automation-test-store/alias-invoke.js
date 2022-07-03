8086a2cb8cf08a9ea04b2baef8e83f1b221f7d4c
jgrabanera <jgrabanera@gmail.com>
2022-07-03 08:03:45 +0800

    comment

--- cypress/integration/automation-test-store/alias-invoke.js
+++ cypress/integration/automation-test-store/alias-invoke.js
@@ -13,7 +13,20 @@ describe("Alias and invoke", () => {
       .as("productThumbnail")
 
     cy.get("@productThumbnail").its("length").should("be.gt", 5) //validate if its length is greater than 5
-
     cy.get("@productThumbnail").should("include", "Seaweed Conditioner")
+  })
+
+  //validate if the number of .thumbnail class is exact 16
+  it.only("Validate product thumbnail", () => {
+    cy.visit("https://automationteststore.com/")
+
+    //Invoke = extract
+    //.as() is use a variable
+    cy.get(".thumbnail").as("productThumbnail")
+    cy.get("@productThumbnail").should("have.length", 16)
+    cy.get("@productThumbnail")
+      .find(".productcart") //Find the class .productcart within the alias @productThumbnail
+      .invoke("attr", "title") // Extract the attribute = title within the line
+      .should("include", "Add to Cart") //include or contain may work
   })
 })
