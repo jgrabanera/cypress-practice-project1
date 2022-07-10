750e44eba79ea8d1b4cde6376ba1d949640cc906
jgrabanera <jgrabanera@gmail.com>
2022-07-11 05:14:41 +0800

    add browser navigation test script

---
+++ cypress/integration/webdriver-uni/browser-navigation.js
@@ -0,0 +1,36 @@
+/// <reference types="Cypress" />
+
+describe("Validate WebdriverUni homepage links", () => {
+  it.only("Confirm links redirect to the correct pages", () => {
+    //cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+    cy.visit("https://webdriveruniversity.com");
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
+    cy.url().should("include", "contactus");
+
+    cy.go("back");
+    cy.reload();
+    //cy.reload(true) // reload without loading cache
+
+    cy.go("forward");
+    cy.url().should("include", "contactus");
+    cy.wait(1000);
+    cy.go("back");
+
+    cy.get("#login-portal")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.url().should("include", "Login-Portal");
+    cy.wait(1000);
+    cy.go("back");
+
+    cy.get("#to-do-list").invoke("removeAttr", "target").click({ force: true });
+    cy.url().should("include", "To-Do-List");
+    cy.wait(1000);
+    cy.go("back");
+
+    cy.get("#to-do-list").invoke("removeAttr", "target").click({ force: true });
+    cy.url().should("include", "To-Do-List");
+    cy.wait(1000);
+    cy.go("back");
+  });
+});
