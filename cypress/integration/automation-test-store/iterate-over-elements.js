7159b33846b0ea31909243b80b6383bc770cf49f
jgrabanera <jgrabanera@gmail.com>
2022-08-01 21:53:28 +0800

    custom command (method) update

--- cypress/integration/automation-test-store/iterate-over-elements.js
+++ cypress/integration/automation-test-store/iterate-over-elements.js
@@ -1,31 +1,28 @@
 /// <reference types="cypress" />
 
-describe("Iterate over elements", () => {
-  it("Log information of all hair care products", () => {
-    cy.visit("https://automationteststore.com/")
-    cy.get("a[href*='product/category&path=']").contains("Hair Care").click()
+describe('Iterate over elements', () => {
+  it('Log information of all hair care products', () => {
+    cy.visit('https://automationteststore.com/');
+    cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
 
-    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
-      cy.log("Index: " + index + " : " + $el.text())
-    })
-  })
-  it.only("Add specific product to basket", () => {
-    cy.visit("https://automationteststore.com/")
-    cy.get("a[href*='product/category&path=']").contains("Hair Care").click()
+    cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+      cy.log('Index: ' + index + ' : ' + $el.text());
+    });
+  });
+  it.only('Add specific product to basket', () => {
+    cy.visit('https://automationteststore.com/');
+    cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
 
-    cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
-      if ($el.text().includes("Eau Parfumee au The Vert Shampoo")) {
-        // wrap this element so we can
-        // use cypress commands on it
-        cy.wrap($el).click()
+    // cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+    //   if ($el.text().includes('Eau Parfumee au The Vert Shampoo')) {
+    //     cy.wrap($el).click();
 
-        cy.log("Index: " + index + " : " + $el.text() + " Test completed ")
-      } else {
-        // do something else
-      }
+    //     cy.log('Index: ' + index + ' : ' + $el.text() + ' Test completed ');
+    //   }
 
-      //const firstNameText = text.find("#field_11").text()
-      //cy.log("Index: " + index + " : " + $el.text())
-    })
-  })
-})
+    cy.selectProduct('Eau Parfumee au The Vert Shampoo');
+
+    //const firstNameText = text.find("#field_11").text()
+    //cy.log("Index: " + index + " : " + $el.text())
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,6 +20,16 @@
 // -- This is a dual command --
 // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
 //
+Cypress.Commands.add('selectProduct', (productName) => {
+  cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+    if ($el.text().includes(productName)) {
+      cy.wrap($el).click();
+
+      cy.log('Index: ' + index + ' : ' + $el.text() + ' Test completed ');
+    }
+  });
+});
+
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
