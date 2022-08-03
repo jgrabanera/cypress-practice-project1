e8839ad0a0f09470a880773411fb74faa439b476
jgrabanera <jgrabanera@gmail.com>
2022-08-03 18:06:38 +0800

    improve code with custom commands to lessen the lines of codes

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -13,33 +13,17 @@ describe('Test Contact Us Form via WebdriverUni', () => {
     cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true });
   });
 
-  it.only('Should be able to submit a successful submission via contact us form', () => {
+  it('Validate active document properties', () => {
     cy.document().should('have.property', 'charset').and('eq', 'UTF-8'); //validate active document properties
     cy.title().should('include', 'WebDriver | Contact Us');
     cy.url().should('include', 'contactus');
-    //cy.get("#contact-us").click({ force: true })
-    cy.get('[name="first_name"]').type(data.first_name);
-    cy.get('[name="last_name"]').type(data.last_name);
-    cy.get('[name="email"]').type(data.email);
-    cy.get('[name="email"]').should('have.attr', 'name', 'email'); //validate email input
+  });
 
-    cy.get('textarea.feedback-input').type(data.body);
-    cy.get('[type="submit"]').click();
-
-    cy.get('h1').should('have.text', 'Thank You for your Message!');
+  it('Should be able to submit a successful submission via contact us form', () => {
+    cy.webdrivderUni_ContactForm_Submit(data.first_name, data.last_name, data.email, data.body, 'h1', 'Thank You for your Message!');
   });
 
   it('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
-    //Cypress code
-    //cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html");
-    // cy.visit('https://webdriveruniversity.com');
-    // cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true });
-
-    cy.get('[name="first_name"]').type('Joe');
-    cy.get('[name="last_name"]').type("Doesn't");
-    cy.get('textarea.feedback-input').type('Test message');
-    cy.get('[type="submit"]').click();
-
-    cy.get('body').contains('Error: all fields are required');
+    cy.webdrivderUni_ContactForm_Submit(data.first_name, data.last_name, ' ', data.body, 'body', 'Error: Invalid email address');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,7 +29,17 @@ Cypress.Commands.add('selectProduct', (productName) => {
     }
   });
 });
+// ---------------END OF THE LINE---------------------
 
+// ----------------WEBDRIVER UNI------------
+Cypress.Commands.add('webdrivderUni_ContactForm_Submit', (first_name, last_name, email, textMsg, $selector, successMsg) => {
+  cy.get('[name="first_name"]').type(first_name);
+  cy.get('[name="last_name"]').type(last_name);
+  cy.get('[name="email"]').type(email);
+  cy.get('textarea.feedback-input').type(textMsg);
+  cy.get('[type="submit"]').click();
+  cy.get($selector).contains(successMsg);
+});
 // ---------------END OF THE LINE---------------------
 
 //
