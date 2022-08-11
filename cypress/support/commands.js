b9806af0db49801627b524b8c4ee2517ec88c029
jgrabanera <jgrabanera@gmail.com>
2022-08-11 20:31:17 +0800

    update Cypress Commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -43,19 +43,6 @@ Cypress.Commands.add('addProductToBasket', (productName) => {
 
 // ----------------WEBDRIVER UNI------------
 
-// This is added on Homepage Page Object
-// Cypress.Commands.add('navigateToContactPage', () => {
-//   cy.visit('/' + '/Contact-Us/contactus.html');
-// });
-
-Cypress.Commands.add('webdrivderUni_ContactForm_Submit', (first_name, last_name, email, textMsg, $selector, successMsg) => {
-  cy.get('[name="first_name"]').type(first_name);
-  cy.get('[name="last_name"]').type(last_name);
-  cy.get('[name="email"]').type(email);
-  cy.get('textarea.feedback-input').type(textMsg);
-  cy.get('[type="submit"]').click();
-  cy.get($selector).contains(successMsg);
-});
 // ---------------END OF THE LINE---------------------
 
 //
