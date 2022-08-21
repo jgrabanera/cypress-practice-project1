2362279c164997191cfab9f8e5d3298f39722e74
jgrabanera <jgrabanera@gmail.com>
2022-08-21 17:51:59 +0800

    update automation test store Page Object and removed constants file

--- cypress/integration/automation-test-store/add-multiple-items-to-basket.js
+++ cypress/integration/automation-test-store/add-multiple-items-to-basket.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
-import { constants } from '../../support/constants';
+import AutoStore_Homepage_PO from '../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO';
 
 describe('Add multiple items to basket', () => {
+  const autostore_Homepage_PO = new AutoStore_Homepage_PO();
   before(() => {
     cy.fixture('products').then(function (data) {
       globalThis.data = data;
@@ -9,9 +10,9 @@ describe('Add multiple items to basket', () => {
   });
 
   beforeEach(() => {
-    cy.visit(constants.automationTestStore_Url);
-    cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
+    autostore_Homepage_PO.AutoStore_Homepage_PO();
   });
+
   it('Add specific items to basket', () => {
     globalThis.data.productName.forEach((element) => {
       cy.addProductToBasket(element);
--- cypress/support/constants.js
+++
@@ -1,4 +0,0 @@
-//for Automation Test Store Site
-export const constants = {
-  automationTestStore_Url: 'https://automationteststore.com/',
-};
