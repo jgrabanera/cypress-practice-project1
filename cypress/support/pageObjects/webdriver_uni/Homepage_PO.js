acdd6a8d30a5f2e6738ef8ab136a200e0e56b5f0
jgrabanera <jgrabanera@gmail.com>
2022-08-21 22:31:02 +0800

    add timeout on click

--- cypress/support/pageObjects/webdriver_uni/Homepage_PO.js
+++ cypress/support/pageObjects/webdriver_uni/Homepage_PO.js
@@ -1,11 +1,11 @@
 class HomePage_PO {
   visitHomepage() {
     //cy.visit('/');  use as base URL
-    cy.visit(Cypress.env('webdriveruni_homepage'), { timeout: 60000 }); //use the environment variable
+    cy.visit(Cypress.env('webdriveruni_homepage'), { timeout: 60000 }); //use the environment variable and add specific timeout on the page
   }
 
   clickOn_ContactUs_Button() {
-    cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true });
+    cy.get('#contact-us').invoke('removeAttr', 'target').click({ force: true }, { timeout: 60000 }); // add specific timeout on Click
   }
 }
 export default HomePage_PO;
