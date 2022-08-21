fc53f35f5d4e3fc686c7e207c706a945a0d105c4
jgrabanera <jgrabanera@gmail.com>
2022-08-21 17:53:29 +0800

    add automation test store page object file

---
+++ cypress/support/pageObjects/automation-test-store/AutoStore_Homepage_PO.js
@@ -0,0 +1,11 @@
+class AutoStore_Homepage_PO {
+  accessHomepage() {
+    cy.visit('https://automationteststore.com/');
+  }
+
+  clickOn_HairCare_Link() {
+    cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
+  }
+}
+
+export default AutoStore_Homepage_PO;
