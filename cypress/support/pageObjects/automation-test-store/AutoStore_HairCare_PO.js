7cf3d8bd77e68d43e8450c8f2b57a0ee7cde2787
jgrabanera <jgrabanera@gmail.com>
2022-08-21 23:48:33 +0800

    add cypress debugger

--- cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
+++ cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
@@ -1,7 +1,9 @@
 class AutoStore_HairCare_PO {
   addHairCareProductToBasket() {
     globalThis.data.productName.forEach((element) => {
-      cy.addProductToBasket(element);
+      cy.addProductToBasket(element).then(() => {
+        debugger;
+      });
     });
     cy.get('.dropdown-toggle > .fa').click({ force: true });
   }
