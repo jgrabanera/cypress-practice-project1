6ea7e99575f59e01396a6db64f525a620afa4e75
jgrabanera <jgrabanera@gmail.com>
2022-08-29 18:30:28 +0800

    update PO and variables

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -1,9 +1,12 @@
 /// <reference types="cypress" />
-import { constants } from '../../support/constants';
+import AutoStore_Homepage_PO from '../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO';
 
 describe('Verifying variables, cypress commands and jquery commands', () => {
+  const autostore_Homepage_PO = new AutoStore_Homepage_PO();
+
   it('Navigating to specific product pages', () => {
-    cy.visit(constants.automationTestStore_Url);
+    autostore_Homepage_PO.accessHomepage();
+    //cy.visit(constants.automationTestStore_Url);
     //The following will fail
     // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup")
     // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare")
@@ -22,7 +25,8 @@ describe('Verifying variables, cypress commands and jquery commands', () => {
   });
 
   it('Navigating to specific product pages', () => {
-    cy.visit(constants.automationTestStore_Url);
+    autostore_Homepage_PO.accessHomepage();
+    // cy.visit(constants.automationTestStore_Url);
     cy.get("a[href*='product/category&path=']").contains('Makeup').click();
 
     //Following code will fail
@@ -37,8 +41,9 @@ describe('Verifying variables, cypress commands and jquery commands', () => {
     });
   });
 
-  it.only('Validate properties of Contact Us Page', () => {
-    cy.visit('https://automationteststore.com/index.php?rt=content/contact/');
+  it('Validate properties of Contact Us Page', () => {
+    autostore_Homepage_PO.clickOn_Contact_Us_Page();
+    //cy.visit('https://automationteststore.com/index.php?rt=content/contact/');
 
     //Cypress approach
     cy.contains('#ContactUsFrm', 'Contact Us Form').find('#field_11').should('contain', 'First name');
--- cypress/support/pageObjects/automation-test-store/AutoStore_Homepage_PO.js
+++ cypress/support/pageObjects/automation-test-store/AutoStore_Homepage_PO.js
@@ -6,6 +6,10 @@ class AutoStore_Homepage_PO {
   clickOn_HairCare_Link() {
     cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
   }
+
+  clickOn_Contact_Us_Page() {
+    cy.visit('https://automationteststore.com/index.php?rt=content/contact/');
+  }
 }
 
 export default AutoStore_Homepage_PO;
