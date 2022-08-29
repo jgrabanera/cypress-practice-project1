6795f1a25adeb96343a80083dfc7ae62cc4c7c07
jgrabanera <jgrabanera@gmail.com>
2022-08-29 17:43:31 +0800

    update hooks

--- cypress/integration/automation-test-store/inspect-item.js
+++ cypress/integration/automation-test-store/inspect-item.js
@@ -1,14 +1,20 @@
 /// <reference types="Cypress" />
-import { constants } from '../../support/constants';
+import AutoStore_Homepage_PO from '../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO';
 
 describe('Inspect Automation Test Store items using chain of commands', () => {
+  const autostore_Homepage_PO = new AutoStore_Homepage_PO();
+
+  beforeEach(() => {
+    autostore_Homepage_PO.accessHomepage();
+  });
+
   it('Click on the first item using item header', () => {
-    cy.visit(constants.automationTestStore_Url);
+    // cy.visit(constants.automationTestStore_Url); replace by beforeEach Hook
     cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click();
   });
 
-  it.only('Click on the first item using item text', () => {
-    cy.visit(constants.automationTestStore_Url);
+  it('Click on the first item using item text', () => {
+    // cy.visit(constants.automationTestStore_Url); replace by beforeEach Hook
     cy.get('.prdocutname')
       .contains('Skinsheen Bronzer Stick')
       .click()
@@ -19,7 +25,7 @@ describe('Inspect Automation Test Store items using chain of commands', () => {
 
   //Command chain to easy get the product based on the list index
   it('Click on the first item using index', () => {
-    cy.visit(constants.automationTestStore_Url);
+    // cy.visit(constants.automationTestStore_Url); replace by beforeEach Hook
     cy.get('.fixed_wrapper').find('.prdocutname').eq(0).click();
   });
 });
