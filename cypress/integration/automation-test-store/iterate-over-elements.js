8a04f86e0a2324f6eea5c71c113e5b54757511ab
jgrabanera <jgrabanera@gmail.com>
2022-08-29 18:43:43 +0800

    update iterate over elements

--- cypress/integration/automation-test-store/iterate-over-elements.js
+++ cypress/integration/automation-test-store/iterate-over-elements.js
@@ -1,18 +1,26 @@
 /// <reference types="cypress" />
-import { constants } from '../../support/constants';
+import AutoStore_Homepage_PO from '../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO';
 
 describe('Iterate over elements', () => {
+  const autostore_Homepage_PO = new AutoStore_Homepage_PO();
+  const autostore_Haircare_PO = new AutoStore_HairCare_PO();
+
+  beforeEach(() => {
+    autostore_Homepage_PO.accessHomepage();
+    autostore_Homepage_PO.clickOn_HairCare_Link();
+  });
+
   it('Log information of all hair care products', () => {
-    cy.visit(constants.automationTestStore_Url);
-    cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
+    //cy.visit(constants.automationTestStore_Url);
+    //cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
 
     cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
       cy.log('Index: ' + index + ' : ' + $el.text());
     });
   });
   it.only('Add specific product to basket', () => {
-    cy.visit(constants.automationTestStore_Url);
-    cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
+    //cy.visit(constants.automationTestStore_Url);
+    //cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
 
     cy.selectProduct('Eau Parfumee au The Vert Shampoo');
   });
