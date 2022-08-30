07d27554cabbab937b5a36805c4faac8eddabbd3
jgrabanera <jgrabanera@gmail.com>
2022-08-30 21:12:29 +0800

    add merge results in JUnit and update some scripts

--- cypress/integration/automation-test-store/inspect-item.js
+++ cypress/integration/automation-test-store/inspect-item.js
@@ -9,12 +9,12 @@ describe('Inspect Automation Test Store items using chain of commands', () => {
   });
 
   it('Click on the first item using item header', () => {
-    // cy.visit(constants.automationTestStore_Url); replace by beforeEach Hook
+    // cy.visit(constants.automationTestStore_Url); //replace by beforeEach Hook
     cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click();
   });
 
   it('Click on the first item using item text', () => {
-    // cy.visit(constants.automationTestStore_Url); replace by beforeEach Hook
+    // cy.visit(constants.automationTestStore_Url); //replace by beforeEach Hook
     cy.get('.prdocutname')
       .contains('Skinsheen Bronzer Stick')
       .click()
@@ -25,7 +25,7 @@ describe('Inspect Automation Test Store items using chain of commands', () => {
 
   //Command chain to easy get the product based on the list index
   it('Click on the first item using index', () => {
-    // cy.visit(constants.automationTestStore_Url); replace by beforeEach Hook
+    // cy.visit(constants.automationTestStore_Url); //replace by beforeEach Hook
     cy.get('.fixed_wrapper').find('.prdocutname').eq(0).click();
   });
 });
