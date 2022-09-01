ed5861e1365b4a40cd5f7cc3432056a4097f68f2
jgrabanera <jgrabanera@gmail.com>
2022-09-02 07:37:24 +0800

    overriding Cypress Test Runner configuration file using staging.json file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,11 +12,26 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+// plugins file
+// promisified fs module
+const fs = require('fs-extra');
+const path = require('path');
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress', 'cypress/config', `${file}.json`); //directory
+
+  if (!fs.existsSync(pathToConfigFile)) {
+    console.log('No custom config file found.');
+    return {};
+  }
+
+  return fs.readJson(pathToConfigFile);
 }
+
+// plugins file
+module.exports = (on, config) => {
+  // accept a configFile value or use development by default
+  const file = config.env.configFile;
+
+  return getConfigurationByFile(file);
+};
